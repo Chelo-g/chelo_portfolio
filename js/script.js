35ba054acff27f1f4e151158813e30804f7b25d8
Chelo <cheese8533@gmail.com>
2021-06-01 21:46:51 +0800

    キャッチタイトル、ナビのtext-stroke反映されない問題解消
    [fix] navのtext-strokeはaタグで使用時、:visitedが優先されるのでvisitedも入力する
    [add] ヘッダーストーキング機能追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var logoclass = ".logo";
-
+var logoclass2 = ".top-view__ruby";
 /* make span for each character */
 $(logoclass)
   .children()
@@ -11,3 +11,25 @@ $(logoclass)
       $this.replaceWith($this.text().replace(/(\S)/g, "<span>$&</span>"));
     }
   });
+
+$(logoclass2)
+  .children()
+  .addBack()
+  .contents()
+  .each(function () {
+    if (this.nodeType == 3) {
+      var $this = $(this);
+      $this.replaceWith($this.text().replace(/(\S)/g, "<span>$&</span>"));
+    }
+  });
+/*  */
+$(function () {
+  var headerHeight = 100; //固定ヘッダーの高さを入れる
+  $('[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - headerHeight;
+    $("html, body").animate({ scrollTop: position }, 500, "swing"); //200はスクロールの移動スピードです
+    return false;
+  });
+});
