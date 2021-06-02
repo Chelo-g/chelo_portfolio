7cb29c8614e6438e1326d4f97fc0e963ead362d1
Chelo <cheese8533@gmail.com>
2021-06-02 16:56:37 +0800

    バーガーメニュー実装、JS一部修正
    [add] バーガーメニュー実装
    [add] レスポンシブ時にスクロールするJS挙動修正

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-var logoclass = ".logo";
-var logoclass2 = ".top-view__ruby";
 /* make span for each character */
+var logoclass = ".logo";
 $(logoclass)
   .children()
   .addBack()
@@ -12,24 +11,35 @@ $(logoclass)
     }
   });
 
-$(logoclass2)
-  .children()
-  .addBack()
-  .contents()
-  .each(function () {
-    if (this.nodeType == 3) {
-      var $this = $(this);
-      $this.replaceWith($this.text().replace(/(\S)/g, "<span>$&</span>"));
-    }
-  });
-/*  */
+/* scroll goto id */
 $(function () {
-  var headerHeight = 100; //固定ヘッダーの高さを入れる
+  var headerHeight = 100;
+  var scroll_speed = 500;
   $('[href^="#"]').click(function () {
+    if (is_displayed_burger == true) {
+      headerHeight = 50;
+    }
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "html" : href);
     var position = target.offset().top - headerHeight;
-    $("html, body").animate({ scrollTop: position }, 500, "swing"); //200はスクロールの移動スピードです
+    $("html, body").animate({ scrollTop: position }, scroll_speed, "swing");
+    if (is_displayed_burger == true) {
+      toggle_burger_menu();
+    }
     return false;
   });
+});
+
+/* buger script */
+var btn_burger = ".btn-burger";
+var nav_class = ".nav";
+var nav_display_class = "nav--is-active";
+var is_displayed_burger = false;
+function toggle_burger_menu() {
+  is_displayed_burger = !is_displayed_burger;
+  $(nav_class).toggleClass(nav_display_class);
+}
+
+$(btn_burger).on("click", function () {
+  toggle_burger_menu();
 });
